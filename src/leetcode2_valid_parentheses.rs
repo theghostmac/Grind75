23450f0ff3eea10d0bce00ee98fe999934732369
theghostmac <theghostmac@gmail.com>
2023-06-08 23:54:27 +0100

    did the valid parentheses problem with tests

--- src/leetcode2_valid_parentheses.rs
+++ src/leetcode2_valid_parentheses.rs
@@ -1,3 +1,40 @@
 pub fn is_valid(s: String) -> bool {
-    true
+    let mut stack = "".to_string();
+
+    for ch in s.chars() {
+        match ch {
+            '(' => stack.push(')'),
+            '{' => stack.push('}'),
+            '[' => stack.push(']'),
+            ')' | '}' | ']' => {
+                if let Some(m) = stack.pop() {
+                    if m != ch {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            _ => return false,
+        }
+    }
+    stack.len() == 0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let s: String = String::from("()");
+        let result = is_valid(s);
+        assert!(result);
+    }
+
+    fn example2() {
+        let s: String = String::from("()");
+        let result = is_valid(s);
+        assert!(result);
+    }
 }
